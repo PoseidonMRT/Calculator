68b1fa131c154e8362d270bce9233d765ca0fa6e
tuozhaobing <1044176601@qq.com>
2017-10-24 16:05:09 +0800

    init project commit

---
+++ ReactCalculatorMain.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import {
+    Text,
+    View,
+} from 'react-native';
+
+export default class ReactCalculator extends Component {
+    render() {
+        return (
+            <View style={{flex: 1}}>
+                <View style={{flex: 2, backgroundColor: '#193441'}}></View>
+                <View style={{flex: 8, backgroundColor: '#3E606F'}}></View>
+            </View>
+        )
+    }
+}
---
+++ index.js
@@ -0,0 +1,4 @@
+import { AppRegistry } from 'react-native';
+import ReactCalculatorMain from './ReactCalculatorMain';
+
+AppRegistry.registerComponent('Calculator', () => ReactCalculatorMain);
